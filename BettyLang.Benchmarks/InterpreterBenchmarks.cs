7e4b4eb3b26af2606e0a6ea1c75e1f698f128f21
Dimitrios Iliadis <dimitrios.iliadis@outlook.com>
2024-02-07 16:47:44 +0200

    Add benchmark project

---
+++ BettyLang.Benchmarks/InterpreterBenchmarks.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using BettyLang.Core;
+
+namespace BettyLang.Benchmarks
+{
+    public class InterpreterBenchmarks
+    {
+        [Benchmark]
+        public void RecursiveFunctionCall()
+        {
+            var code = $$"""
+                func fact(n)
+                {
+                    if (n == 0)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return n * fact(n - 1);
+                    }
+                }
+
+                func main()
+                {
+                    fact(5);
+                }
+                """;
+
+            var lexer = new Lexer(code);
+            var parser = new Parser(lexer);
+            var interpreter = new Interpreter(parser);
+
+            interpreter.Interpret();
+        }
+    }
+}
