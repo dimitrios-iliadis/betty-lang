7a1b23d18a11ca2696aec14a7d7981668a497f09
Dimitrios Iliadis <dimitrios.iliadis@outlook.com>
2024-02-07 19:00:58 +0200

    Refactor existing benchmark class and rename it to `FactorialFunctionBenchmark`

---
+++ BettyLang.Benchmarks/FactorialFunctionBenchmark.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using BettyLang.Core;
+
+namespace BettyLang.Benchmarks
+{
+    public class FactorialFunctionBenchmark
+    {
+        private Interpreter interpreter;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            string code = $$"""
+            func fact(n)
+            {
+                if (n == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return n * fact(n - 1);
+                }
+            }
+
+            func main()
+            {
+                fact(5);
+            }
+            """;
+
+            var lexer = new Lexer(code);
+            var parser = new Parser(lexer);
+            interpreter = new Interpreter(parser);
+        }
+
+        [Benchmark]
+        public void BenchmarkFactorialFunction()
+        {
+            interpreter.Interpret();
+        }
+    }
+}
